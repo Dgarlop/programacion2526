b17aa227f79d4aad2795cc732b6dda267246cdfe
Dgarlop <dgarlop755@g.educaand.es>
2025-10-03 10:55:59 +0200

    Subo arhivos for

--- Python/UD1/expreciones_for/ejercicio3.py
+++ Python/UD1/expreciones_for/ejercicio3.py
@@ -1,8 +1,8 @@
 n=int(input("Intrduce hasta donde quieres sumar:"" "))
 suma = 0 
 if n>0:
-    for i in range(1, n):
-        suma += i 
+    for i in range(1, n +1):
+        suma = suma + i 
     print("La suma es:", suma)
 else:
     print("Fin")
