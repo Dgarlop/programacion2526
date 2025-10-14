97abad3dc9aef212c1e6edcc128d15067bad5b1e
Dgarlop <dgarlop755@g.educaand.es>
2025-10-14 08:38:26 +0200

    Subo archivos

--- Python/UD1/boletin_for_anidados/ejercicio7.py
+++ Python/UD1/boletin_for_anidados/ejercicio7.py
@@ -2,12 +2,12 @@ n = int(input("Introduce un numero: "" "))
 i = "*"
 
 
-for i in range(1, n+1):
-    if i == 1 or i == n :
+for i in range(0, n+1):
+    if i %2 == 0 :
         print("*","#" * (n-2),"*")
     else: 
         cad = ""
-        for j in range(1, n+1):
+        for j in range(n):
             if j %2 == 0 :
              cad += "*"
             else:
