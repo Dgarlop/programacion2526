54594fd23d26db274a7eea44a22208d42289c9b9
Dgarlop <dgarlop755@g.educaand.es>
2025-10-31 12:29:06 +0100

    Subir los archivos

---
+++ Python/cadenas/ejercicio4.py
@@ -0,0 +1,5 @@
+numero = str(input("Introduce el numero: "))
+posicion = str(input("Introduce la posicion: "))# la posicioon es mas pequeña que la longitud de la lista y si pasa eso quiero que me de el caracter de esa posicion 
+for posicion in numero:
+    print (posicion)
+
