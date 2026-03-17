4545479311e7db223adc92ffe0bcbc34a3b7b242
Dgarlop <dgarlop755@g.educaand.es>
2026-03-17 10:00:23 +0100

    Javaa

---
+++ src/main/java/unidad4/Colecciones/BoletinListas/Ejericio2/Model/Estado.java
@@ -0,0 +1,5 @@
+package unidad4.Colecciones.BoletinListas.Ejericio2.Model;
+
+public enum Estado {
+    PRESTADO, LIBRE
+}
---
+++ src/main/java/unidad4/Colecciones/BoletinListas/Ejericio2/Model/Libro.java
@@ -0,0 +1,62 @@
+package unidad4.Colecciones.BoletinListas.Ejericio2.Model;
+
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String genero;
+    private int annoPublicacion;
+    private Estado estado;
+
+    public Libro(String titulo, String autor, String genero, int annoPublicacion, Estado estado) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.annoPublicacion = annoPublicacion;
+        this.estado = estado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnnoPublicacion() {
+        return annoPublicacion;
+    }
+
+    public void setAnnoPublicacion(int annoPublicacion) {
+        this.annoPublicacion = annoPublicacion;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+}
